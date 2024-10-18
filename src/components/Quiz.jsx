7eb26bdc01ef1dc34240956ac932a4669d367972
Martyna <110538495+martinafm@users.noreply.github.com>
2024-10-18 12:43:17 +0200

    made answers show in different order every time

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,40 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import QUESTIONS from '../questions.js';
+import quizCompletedImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const isQuizCompleted = QUESTIONS.length === activeQuestionIndex;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
+  }, [])
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer])
+
+  if (isQuizCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompletedImg} alt="Trophy icon" />
+        <h2>Quiz completed!</h2>
+      </div>
+    );
   }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
